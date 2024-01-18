831f6a835a8de8888c6d819516f5062ed2f3e8d5
John Vundi <johnsonvundy2@gmail.com>
2024-01-18 12:04:34 +0300

    configured json srver

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import { AiOutlinePlus } from "react-icons/ai"
+
+export const AddTask = () => {
+  return (
+    <div>
+        <button className="btn btn-primary w-full"> Add new Task
+            <AiOutlinePlus className="ml-2" size={18}/>
+        </button>
+    </div>
+  )
+}
