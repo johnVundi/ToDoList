713a94efae95a7c1073ec27c39796690e53fdf00
John Vundi <johnsonvundy2@gmail.com>
2024-01-18 12:51:15 +0300

    connected the api

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,25 +1,28 @@
-export const TodoList = () => {
+import { ITask } from "@/types/tasks";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+export const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table">
-        {/* head */}
         <thead>
           <tr>
             <th></th>
             <th>Name</th>
-        
             <th>Favorite Color</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr className="bg-base-200">
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Blue</td>
-          
-          </tr>
- 
+          {tasks.map((task) => (
+            <tr key={task.id} className="bg-base-200">
+              <th>1</th>
+              <td>{task.text}</td>
+              <td>Blue</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import { getAllTodos } from "@/api";
 import { AddTask } from "./components/AddTask";
 import { TodoList } from "./components/TodoList";
 
 
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks)
+
+
   return (
     <main className="max-w-4xl mx-auto  mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">To do List</h1>
         <AddTask />
         </div>
-        <TodoList />
+        <TodoList tasks={tasks}/>
     </main>
   );
 }
