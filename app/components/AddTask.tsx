61fb9767115fce45b66c0cefb0f32dbd91dfc4cc
John Vundi <johnsonvundy2@gmail.com>
2024-01-22 09:59:06 +0300

    added the modal

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,11 +1,17 @@
 import { AiOutlinePlus } from "react-icons/ai"
+import { Modal } from "./Modal"
+import { useState } from "react"
 
 export const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+
   return (
     <div>
         <button className="btn btn-primary w-full"> Add new Task
             <AiOutlinePlus className="ml-2" size={18}/>
         </button>
+        <Modal />
     </div>
   )
 }
