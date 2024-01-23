da36c076c65ac9971c0ff22f0f9993088d7d525e
John Vundi <johnsonvundy2@gmail.com>
2024-01-23 10:12:44 +0300

    made the modal functional

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 
 interface ModalProps {
   modalOpen: boolean;
   setModalOpen: () => void;
 }
 
-export const Modal:React.FC<ModalProps> = ({modalOpen, setModalOpen}) => {
+export const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen }) => {
   return (
     <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button onClick={() => setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
-  </div>
-  )
-}
+      <div className="modal-box relative">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button
+            onClick={() => setModalOpen(false)}
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          >
+            ✕
+          </button>
+        </form>
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+      </div>
+    </div>
+  );
+};
